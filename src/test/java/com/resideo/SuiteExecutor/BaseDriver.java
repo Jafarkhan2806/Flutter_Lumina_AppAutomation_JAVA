40b7c985208d442cd464c92b05df2107ba4be6e7
Khan, Mohammed <Jafar.Khan@resideo.com>
2020-02-06 13:38:20 -0600

    modified base class
    modified base class

--- src/test/java/com/resideo/SuiteExecutor/BaseDriver.java
+++ src/test/java/com/resideo/SuiteExecutor/BaseDriver.java
@@ -4,11 +4,7 @@ import java.net.URL;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import com.honeywell.commons.coreframework.Keyword;
 import com.honeywell.commons.coreframework.SuiteConstants;
-import com.honeywell.commons.coreframework.SuiteUtils;
-import com.honeywell.commons.coreframework.TestCaseInputs;
 import com.honeywell.commons.coreframework.SuiteConstants.SuiteConstantTypes;
 
 import io.appium.java_client.AppiumDriver;
@@ -20,7 +16,7 @@ public  class BaseDriver  {
 
 	public AppiumDriver<MobileElement> driver;
 	public WebDriverWait wait;
-	private TestCaseInputs inputs;
+
 	
 
 	public AppiumDriver<MobileElement> setUp() throws Exception {
@@ -38,12 +34,12 @@ public  class BaseDriver  {
 		} else{
 			DesiredCapabilities capabilities = new DesiredCapabilities();
 			capabilities.setCapability("platformName", "iOS");
-			capabilities.setCapability("platformVersion", "13.2.3");
-			capabilities.setCapability("deviceName", "iPhone 8");
+			capabilities.setCapability("platformVersion", "13.3.1");
+			capabilities.setCapability("deviceName", "iPhone X");
 			capabilities.setCapability("udid", "c85d7cb996a05d26abb89c93df86d62af5486f1f");
 			capabilities.setCapability("bundleId", "com.resideo.rezi.wld.");
 			capabilities.setCapability("noReset", true);
-			capabilities.setCapability("app", "/Users/Resideo/Downloads/Runner.app");
+			capabilities.setCapability("app", "/Users/e844455/Documents/LANTERN_APP/LUMINA_APP/iOS/Debug.app");
 			capabilities.setCapability("automationName", "Flutter");
 			capabilities.setCapability("newCommandTimeout", "36000");
 			driver = new IOSDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
